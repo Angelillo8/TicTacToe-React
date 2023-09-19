6c813fcc71ee528b12468ba6e842150d8ffd2d86
Angel <angelgonzc@yahoo.com>
2023-09-19 18:12:49 +0200

    board, cells, circles and crosses

--- src/Components/TicTacToe/Cell.jsx
+++ src/Components/TicTacToe/Cell.jsx
@@ -1,3 +1,30 @@
+import './TicTacToe.css';
 
+const Cell = ({position, cell, setCells, player, setPlayer}) => {
 
-arr
+    const handleClick = (event) => {
+        const cellPlayed = event.target.firstChild.classList.contains("circle") || 
+                           event.target.firstChild.classList.contains("cross") 
+
+    console.log('cellPlayed :>> ', cellPlayed);
+        if(!cellPlayed) {
+            if (player === "circle") {
+                event.target.firstChild.classList.add("circle")
+                setPlayer("cross")
+            }
+            else {
+                event.target.firstChild.classList.add("cross")
+                setPlayer("circle")
+            }
+        }
+
+    }
+
+    return (
+        <div className="cell" id={position} onClick={handleClick}>
+            <div className={cell}></div>
+        </div>
+    );
+};
+
+export default Cell;
--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -1,12 +1,36 @@
+import { useState } from 'react';
 import './TicTacToe.css';
+import Cell from './Cell';
 
 const TicTacToe = () => {
 
+    const randomChoice = (arrayToChoose) => {
+        return Math.floor(Math.random() * arrayToChoose.length)
+    }
+
+    const circleOrCross = ["circle", "cross"]
+    const [cells, setCells] = useState(["","","","","","","","",""])
+    const [player, setPlayer] = useState(circleOrCross[(randomChoice(circleOrCross))])
+
+    const message = "It is now " + player + "'s turn."
+
     return (
-        <div>
-        
+        <div className="container">
+            <h1 className="title">Tic Tac Toe</h1>
+            <div className="board">
+                {cells.map((cell, index) => 
+                <Cell
+                    key={index}
+                    position={index}
+                    cell={cell}
+                    setCells={setCells}
+                    player={player}
+                    setPlayer={setPlayer}
+                />)}
+            </div>
+            <button className="reset">Reset</button>
         </div>
     )
-};
+}
 
 export default TicTacToe;
